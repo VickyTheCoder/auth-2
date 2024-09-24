0f41b533b71b457ec3acf469e9260ebe4d3a7495
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2024-09-25 00:36:33 +0530

    Added Login Form

--- ui/views.py
+++ ui/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from cust.forms import LoginForm 
 # Create your views here.
 def login_page(request):
     if request.method == 'GET':
         return render(request=request,
-                      template_name='login.html')
+                      template_name='login.html',
+                      context={'login_form': LoginForm})
